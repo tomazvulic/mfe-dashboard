b46cf4a83d2c8f2ed633d91077f66211825433a1
Tomaž Vulič <tomaz.vulic@gmail.com>
2021-12-17 09:37:45 +0100

    container change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <>
-            <h1>Container App!!</h1>
+            <h1>Container App!1!</h1>
             <hr />
             <MarketingApp />
         </>
